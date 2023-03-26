b8c370c5199d1d8f33fa55a95b96cef70ad6cb5d
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-03-26 21:52:57 +0200

    create_matrices and fill_matrices finished

--- src/create_matrices.py
+++ src/create_matrices.py
@@ -1,3 +1,53 @@
-def create_matrices():
-  """docstring"""
-  pass
+# creation matrices
+
+def create_matrices(n):
+    """creation of the six triangular matrices """
+    global vx, wx, vhx, whx, yhx, zhx
+    vx = [[None for i in range(j+1)] for j in range(n)]
+    wx = [[None for i in range(j+1)] for j in range(n)]
+    vhx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
+    whx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
+    yhx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
+    zhx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
+
+
+def fill_matrices():
+    """fill the matrices with the initialization conditions"""
+    n = len(vx)
+
+    # initialization for vx and wx
+    for i in range(n):
+        vx[i][i] = float('inf')
+        wx[i][i] = 0
+
+    #initialization for vhx, whx, yhx, zhx
+    for j in range(n):
+        for k in range(j+1):
+            for i in range(k+1):
+
+                #vhx
+                vhx[j][k][k][i] = float('inf')
+                
+                #yhx
+                yhx[j][k][k][i] = float('inf')
+                
+                # whx
+                whx[j][k][k][i] = wx[j][i]
+                whx[j][j][i][i] = float('inf')
+                #k+1 must be lower than j
+                if k+1 <= j:
+                    whx[j][k+1][k][i] = wx[j][i]
+                
+                #zhx
+                zhx[j][k][k][i] = vx[j][i]
+                #k+1 must be lower than j 
+                if k+1 <= j:
+                    zhx[j][k+1][k][i] = vx[j][i]
+                
+
+                
+
+create_matrices(4)
+fill_matrices()
+#for l in vx: print(l)
+#for l in wx: print(l)
