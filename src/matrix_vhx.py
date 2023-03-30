92e261b8146750b155a75c99ba7ae025fe2fd97f
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-03-30 13:23:17 +0200

    matrix_vhx finished
    to do : import & docstring

---
+++ src/matrix_vhx.py
@@ -0,0 +1,35 @@
+# matrix vhx
+
+def matrix_vhx(i, j, k, l)
+    """docstring"""
+
+    #check indices
+    if (i > k) or (k > l) or (l > j):
+        print(f"Error : invalid index. \n i = {i}, k = {k}, l = {l}, j = {j}")
+        exit(1)
+
+    if vhx[j][l][k][i] is not None:
+        return vhx[j][l][k][i]
+
+
+    # initialization of the optimal score
+    best_score = float('inf')
+
+
+    # search for a better score
+    if score := EIS2_wave(i, j, k, l) < best_score: best_score = score
+    if score := (2 * P_wave + M_wave + matrix_whx(i+1; j-1, k-1, l+1)) < best_score: best_score =
+    score
+
+
+    for s in range(l, j+1):
+        for r in range(i, k+1):
+            if score := EIS2_wave(i, j, r s) + matrix_vhx(r, s, k, l) < best_score: best_score =
+            score
+            if score := EIS2_wave(r, s k, l) + matrix_vhx(i, j, r, s) < best_score: best_score =
+            score
+
+
+    # store the best score in the matrix and return it 
+    vhx[j][l][k][i] = best_score
+    return best_score
