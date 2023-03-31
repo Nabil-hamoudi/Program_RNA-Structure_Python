87b07769d31029e4d96fd5dfdc535e1435c75be9
Nabil HAMOUDI <nabil.hamoudi@ens.uvsq.fr>
2023-03-31 11:23:27 +0200

    Adding yhx and zhx matrix fonction

--- src/matrix_yhx.py
+++ src/matrix_yhx.py
@@ -1,1 +1,40 @@
+# import matrix_vhx
+# import matrix_wxi
+# import matrix_whx
 
+
+def matrix_yhx(i, j, k, l):
+    """docstring"""
+    if (i > k) or (k > l) or (l > j):
+        print(f"Error : invalid index.\n i = {i}, k = {k}, l = {l}, j= {j} ")
+        exit(1)
+
+    if yhx[j][l][k][i] is not None:
+        return yhx[j][l][k][i]
+
+    # initialization of the optimal score
+    best_score = float('inf')
+
+    # paired
+    if score := (P_wave + matrix_vhx(i, j, k, l)) < best_score: best_score = score
+
+    # single-stranded
+    if score := (Q_wave + matrix_yhx(i + 1, j, k, l)) < best_score: best_score = score
+    if score := (Q_wave + matrix_yhx(i, j - 1, k, l)) < best_score: best_score = score
+
+    # nested bifurcations
+    for r in range(i, k+1):
+        if score := (matrix_wxi(i, r) + matrix_yhx(r+1, j, k, l)) < best_score: best_score = score
+
+    for s in range(l, j+1):
+        if score := (matrix_yhx(i, s, k, l) + matrix_wxi(s+1, j)) < best_score: best_score = score
+
+    for r in range(i, k+1):
+        for s in range(l, j+1):
+            if score := (matrix_yhx(i, j, r, s) + EIS2(r, s, k, l)) < best_score: best_score = score
+
+    if score := (P_wave + M_wave + matrix_whx(i, j, k - 1, l + 1)) < best_score: best_score = score
+
+    # store the best score in the matrix and return it 
+    yhx[j][l][k][i] = best_score
+    return best_score
--- src/matrix_zhx.py
+++ src/matrix_zhx.py
@@ -1,8 +1,10 @@
-from create_matrices import create_matrices
+# import matrix_vhx
+# import matrix_wxi
+# import matrix_whx
 
 
 def matrix_zhx(i, j, k, l):
-    """pipoupipoupipoupipoupipoupipou"""
+    """docstring"""
     if (i > k) or (k > l) or (l > j):
         print(f"Error : invalid index.\n i = {i}, k = {k}, l = {l}, j= {j} ")
         exit(1)
@@ -10,3 +12,29 @@ def matrix_zhx(i, j, k, l):
     if zhx[j][l][k][i] is not None:
         return zhx[j][l][k][i]
 
+    # initialization of the optimal score
+    best_score = float('inf')
+
+    # paired
+    if score := (P_wave + matrix_vhx(i, j, k, l)) < best_score: best_score = score
+
+    # single-stranded
+    if score := (Q_wave + matrix_zhx(i, j, k-1, l)) < best_score: best_score = score
+    if score := (Q_wave + matrix_zhx(i, j, k, l+1)) < best_score: best_score = score
+
+    # nested bifurcations
+    for r in range(i, k+1):
+        if score := (matrix_zhx(i, j, r, l) + matrix_wxi(r+1, k)) < best_score: best_score = score
+
+    for s in range(l, j+1):
+        if score := (matrix_zhx(i, j, k, s) + matrix_wxi(l, s-1)) < best_score: best_score = score
+
+    for r in range(i, k+1):
+        for s in range(l, j+1):
+            if score := (EIS2(i, j, r, s) + matrix_zhx(r, s, k, l)) < best_score: best_score = score
+
+    if score := (P_wave + M_wave + matrix_whx(i + 1, j - 1, k, l)) < best_score: best_score = score
+
+    # store the best score in the matrix and return it 
+    zhx[j][l][k][i] = best_score
+    return best_score
