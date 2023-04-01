da50bf7b8c68946bf6a066c840bec3deb0f0ae85
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-01 23:36:41 +0200

    docstring + parameters + imports

--- src/matrix_yhx.py
+++ src/matrix_yhx.py
@@ -1,10 +1,11 @@
-# import matrix_vhx
-# import matrix_wxi
-# import matrix_whx
+from parameters import * 
+from matrix_vhximport matrix_vhx
+from matrix_wxi import matrix_wxi
+from matrix_whx import matrix_whx
 
 
 def matrix_yhx(i, j, k, l):
-    """docstring"""
+    """return the value of the gap matrix yhx at the given indices"""
     if (i > k) or (k > l) or (l > j):
         print(f"Error : invalid index.\n i = {i}, k = {k}, l = {l}, j= {j} ")
         exit(1)
@@ -16,11 +17,11 @@ def matrix_yhx(i, j, k, l):
     best_score = float('inf')
 
     # paired
-    if score := (P_wave + matrix_vhx(i, j, k, l)) < best_score: best_score = score
+    if score := (parameters["P_wave"] + matrix_vhx(i, j, k, l)) < best_score: best_score = score
 
     # single-stranded
-    if score := (Q_wave + matrix_yhx(i + 1, j, k, l)) < best_score: best_score = score
-    if score := (Q_wave + matrix_yhx(i, j - 1, k, l)) < best_score: best_score = score
+    if score := (parameters["Q_wave"] + matrix_yhx(i + 1, j, k, l)) < best_score: best_score = score
+    if score := (parameters["Q_wave"] + matrix_yhx(i, j - 1, k, l)) < best_score: best_score = score
 
     # nested bifurcations
     for r in range(i, k+1):
@@ -33,7 +34,7 @@ def matrix_yhx(i, j, k, l):
         for s in range(l, j+1):
             if score := (matrix_yhx(i, j, r, s) + EIS2(r, s, k, l)) < best_score: best_score = score
 
-    if score := (P_wave + M_wave + matrix_whx(i, j, k - 1, l + 1)) < best_score: best_score = score
+    if score := (parameters["P_wave"] + parameters["M_wave"] + matrix_whx(i, j, k - 1, l + 1)) < best_score: best_score = score
 
     # store the best score in the matrix and return it 
     yhx[j][l][k][i] = best_score
