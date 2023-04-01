d8eba58a6693092a4260c2a5cf3ffd837d203e1b
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-01 23:42:35 +0200

    parameters + imports + docstring + new return

--- src/matrix_zhx.py
+++ src/matrix_zhx.py
@@ -1,27 +1,31 @@
-# import matrix_vhx
-# import matrix_wxi
-# import matrix_whx
+from parameters import * 
+from matrix_vhx import matrix_vhx
+from matrix_wxi import matrix_wxi
+from matrix_whx import matrix_whx
 
-# zhx(i, j : k, k + 1) = vx(i, j) Vk, i<=k<=j
 
 def matrix_zhx(i, j, k, l):
-    """docstring"""
+    """return the value of the gap matrix zhx at the given indices"""
     if (i > k) or (k > l) or (l > j):
         print(f"Error : invalid index.\n i = {i}, k = {k}, l = {l}, j= {j} ")
         exit(1)
 
     if zhx[j][l][k][i] is not None:
         return zhx[j][l][k][i]
+    
+    if (k+1) == l:
+        zhx[j][l][k][i] = matrix_vx(i, j)
+        return matrix_vx(i, j)
 
     # initialization of the optimal score
     best_score = float('inf')
 
     # paired
-    if score := (P_wave + matrix_vhx(i, j, k, l)) < best_score: best_score = score
+    if score := (parameters["P_wave"] + matrix_vhx(i, j, k, l)) < best_score: best_score = score
 
     # single-stranded
-    if score := (Q_wave + matrix_zhx(i, j, k-1, l)) < best_score: best_score = score
-    if score := (Q_wave + matrix_zhx(i, j, k, l+1)) < best_score: best_score = score
+    if score := (parameters["Q_wave"] + matrix_zhx(i, j, k-1, l)) < best_score: best_score = score
+    if score := (parameters["Q_wave"] + matrix_zhx(i, j, k, l+1)) < best_score: best_score = score
 
     # nested bifurcations
     for r in range(i, k+1):
@@ -32,9 +36,9 @@ def matrix_zhx(i, j, k, l):
 
     for r in range(i, k+1):
         for s in range(l, j+1):
-            if score := (EIS2(i, j, r, s) + matrix_zhx(r, s, k, l)) < best_score: best_score = score
+            if score := (parameters["EIS2"](i, j, r, s) + matrix_zhx(r, s, k, l)) < best_score: best_score = score
 
-    if score := (P_wave + M_wave + matrix_whx(i + 1, j - 1, k, l)) < best_score: best_score = score
+    if score := (parameters["P_wave"] + parameters["M_wave"] + matrix_whx(i + 1, j - 1, k, l)) < best_score: best_score = score
 
     # store the best score in the matrix and return it 
     zhx[j][l][k][i] = best_score
