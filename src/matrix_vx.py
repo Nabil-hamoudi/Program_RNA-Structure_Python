b5f4a9d7697b782727de32238b981a2bc402677f
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-01 23:33:05 +0200

    import + correction + parameters

--- src/matrix_vx.py
+++ src/matrix_vx.py
@@ -1,4 +1,6 @@
-# Importer les matrices
+from parameters import *
+from matrix_wxi import matrix_wxi
+from matrix_whx import matrix_whx
 
 def matrix_vx(i, j):
     """
@@ -7,28 +9,28 @@ def matrix_vx(i, j):
     Exit: best score value
     """
     if (i > j):
-        raise NameError("Invalid index, i must be smaller or equal to j.")
+        raise IndexError("Invalid index, i must be smaller or equal to j.")
 
     if vx[j][i] is not None:
-        return wx[j][i]
+        return vx[j][i]
 
     # IS(1)
-    if score := (EIS1(i,j)) < best_score: best_score = score
+    if score := (paramters["EIS1"](i,j)) < best_score: best_score = score
 
     # IS(2)
     for l in range(i,j+1):
         for k in range(i, l+1):
-            if score := (EIS2(i, j, k, l) + matrix_vx(k, l)) < best_score: best_score = score
+            if score := (parameters["EIS2"](i, j, k, l) + matrix_vx(k, l)) < best_score: best_score = score
 
     # Nested Multiloop
     for k in range(i, j+1):
-        if score := (Pi + M + matrix_wxi(i+1, k) + matrix_wxi(k+1, j-1)) < best_score: best_score = score
+        if score := (parameters["Pi"] + parameters["M"] + matrix_wxi(i+1, k) + matrix_wxi(k+1, j-1)) < best_score: best_score = score
 
     # Non nested multiloop
     for r in range(i, j+1):
         for l in range(i, r+1):
             for k in range(i, l+1):
-                if score := (Pi_wave + M_wave + Gwi + matrix_whx(i+1, r, k, l) + matrix_whx(k+1, j-1, l-1, r+1)) < best_score: best_score = score
+                if score := (parameters["Pi_wave"] + parameters["M_wave"] + parameters["Gwi"] + matrix_whx(i+1, r, k, l) + matrix_whx(k+1, j-1, l-1, r+1)) < best_score: best_score = score
 
     # Addition of the best value in the matrix
     vx[j][i] = best_score
