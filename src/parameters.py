2eb57267736daf201d84fc24c2f5d63ca52cdce9
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-01 23:25:22 +0200

    parameters creation

---
+++ src/parameters.py
@@ -0,0 +1,74 @@
+# parameters : table 2
+# EIS1, EIS2 and Coaxial Stacking --> functions
+
+# dictionnary --> key = parameter, value = score
+parameters = {
+        "EIS1" : EIS1,
+        "EIS2" : EIS2,
+        "C" : coaxial_stacking,
+        "P" : 0,
+        "Q" : 0,
+        "R" : dangle_R,
+        "L" : dangle_L,
+        "Pi" : 0.1,
+        "Qi" : 0.4,
+        "Ri" : dangle_Ri,
+        "Li" : dangle_Li,
+        "M" : 4.6,
+        "g" : 0.83,
+        "EIS2_wave" : EIS2_wave,
+        "C_wave" : coaxial_stacking_wave,
+        "P_wave" : 0.1,
+        "Pi_wave" : 0.1*0.83,
+        "Q_wave" : 0.2,
+        "R_wave" : dangle_R_wave,
+        "L_wave" : dangle_L_wave,
+        "M_wave" : 8.43,
+        "Gw" : 7.0,
+        "Gwi" : 13.0,
+        "Gwh" : 6.0
+}
+
+def EIS1():
+    """docstring"""
+    pass
+
+def EIS2():
+    """docstring"""
+    pass
+
+def coaxial_stacking():
+    """docstring"""
+    pass
+
+def dangle_R():
+    """docstring"""
+    pass
+
+def dangle_L():
+    """docstring"""
+    pass
+
+def dangle_Ri():
+    """docstring"""
+    pass
+
+def dangle_Li():
+    """docstring"""
+    pass
+
+def dangle_R_wave():
+    """docstring"""
+    pass
+
+def dangle_L_wave():
+    """docstring"""
+    pass
+
+def EIS2_wave():
+    """docstring"""
+    pass
+
+def coaxial_stacking_wave():
+    """docstring"""
+    pass
