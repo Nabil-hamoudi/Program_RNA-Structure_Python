1270b9eb2efaaf7e82a54e251bc2220e7f803bb0
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-02 15:17:03 +0200

    functions dangle + coaxial

--- src/parameters.py
+++ src/parameters.py
@@ -29,46 +29,153 @@ parameters = {
         "Gwh" : 6.0
 }
 
-def EIS1():
+def EIS1(i, j):
     """docstring"""
     pass
 
-def EIS2():
+def EIS2(i, j, k, l):
     """docstring"""
     pass
 
-def coaxial_stacking():
+def EIS2_wave(i, j, k, l):
     """docstring"""
     pass
 
-def dangle_R():
-    """docstring"""
-    pass
+def coaxial_stacking(i, j, k, l):
+    """compute and return the coaxial stacking score"""
+    
+    #######
+    # --> #
+    # i k #
+    # | | #
+    # j l #
+    # <-- #
+    #######
 
-def dangle_L():
-    """docstring"""
-    pass
+    #table 2 from 'Improved free-energy parameters for predictions of RNA duplex stability'
+    if i == 'A' and j == 'U' and k == 'A' and l == 'U': return -0.9
+    if i == 'A' and j == 'U' and k == 'U' and l == 'A': return -0.9
+    if i == 'U' and j == 'A' and k == 'A' and l == 'U': return -1.1
+    if i == 'C' and j == 'G' and k == 'A' and l == 'U': return -1.8
+    if i == 'C' and j == 'G' and k == 'U' and l == 'A': return -1.7
+    if i == 'G' and j == 'C' and k == 'A' and l == 'U': return -2.3
+    if i == 'G' and j == 'C' and k == 'U' and l == 'A': return -2.1
+    if i == 'C' and j == 'G' and k == 'G' and l == 'C': return -2.0
+    if i == 'G' and j == 'C' and k == 'C' and l == 'G': return -3.4
+    if i == 'G' and j == 'C' and k == 'G' and l == 'C': return -2.9
+    
+    # initiation 3.4 ?
+
+    # mismatches G-U
+    if i == 'A' and j == 'U' and k == 'G' and l == 'U': return -0.5
+    if i == 'C' and j == 'G' and k == 'G' and l == 'U': return -1.5
+    if i == 'G' and j == 'C' and k == 'G' and l == 'U': return -1.3
+    if i == 'U' and j == 'A' and k == 'G' and l == 'U': return -0.7
+    if i == 'G' and j == 'U' and k == 'G' and l == 'U': return -0.5
+    if i == 'U' and j == 'G' and k == 'G' and l == 'U': return -0.6
+
+    if i == 'A' and j == 'U' and k == 'U' and l == 'G': return -0.7
+    if i == 'C' and j == 'G' and k == 'U' and l == 'G': return -1.5
+    if i == 'G' and j == 'C' and k == 'U' and l == 'G': return -1.9
+    if i == 'U' and j == 'A' and k == 'U' and l == 'G': return -0.5
+    if i == 'G' and j == 'U' and k == 'U' and l == 'G': return -0.5
+    if i == 'U' and j == 'G' and k == 'U' and l == 'G': return -0.5
+
+    return -0.4
+    
+    
+
+def coaxial_stacking_wave(i, j, k, l):
+    """compute and return the coaxial stacking score in pseudoknots """
+    return coaxial_stacking(i, j, k, l) * 0.83
+
+
+def dangle_R(i, j, k):
+    """return the free-energy for unpaired 3' terminal nucleotides"""
+    # page 9/16 --> R^j i, j-1 where j = i, i = j, j-1 = k
+
+    #######
+    # --> #
+    # k i #
+    # |   #
+    # j   #
+    # <-- #
+    #######
+
+    if k != i-1 : raise IndexError("k != i-1")
+    # table 3 from 'Improved free-energy parameters for predictions of RNA duplex stability'
+    if i == 'A' and k == 'A' and j == 'U': return -0.8
+    if i == 'C' and k == 'A' and j == 'U': return -0.5
+    if i == 'G' and k == 'A' and j == 'U': return -0.8
+    if i == 'U' and k == 'A' and j == 'U': return -0.6
+    
+    if i == 'A' and k == 'C' and j == 'G': return -1.7
+    if i == 'C' and k == 'C' and j == 'G': return -0.8
+    if i == 'G' and k == 'C' and j == 'G': return -1.7
+    if i == 'U' and k == 'C' and j == 'G': return -1.2
+
+    if i == 'A' and k == 'G' and j == 'C': return -1.1
+    if i == 'C' and k == 'G' and j == 'C': return -0.4
+    if i == 'G' and k == 'G' and j == 'C': return -1.3
+    if i == 'U' and k == 'G' and j == 'C': return -0.6
+    
+    if i == 'A' and k == 'U' and j == 'A': return -0.7
+    if i == 'C' and k == 'U' and j == 'A': return -0.1
+    if i == 'G' and k == 'U' and j == 'A': return -0.7
+    if i == 'U' and k == 'U' and j == 'A': return -0.1
+    
+
+def dangle_L(i, k, j):
+    """return the free-energy for unpaired 5' terminal nucleotides """
+   # page 9/16 --> L^i i+1, j where i = i, i+1 = k, j = j
+   #######
+    # --> #
+    # i k #
+    #   | #
+    #   j #
+    # <-- #
+    #######
+
+    if k != i+1 : raise IndexError("k != i+1")
+    # table 3 from 'Improved free-energy parameters for predictions of RNA duplex stability'
+    if i == 'A' and k == 'A' and j == 'U': return -0.3
+    if i == 'C' and k == 'A' and j == 'U': return -0.3
+    if i == 'G' and k == 'A' and j == 'U': return -0.3
+    if i == 'U' and k == 'A' and j == 'U': return -0.2
+    
+    if i == 'A' and k == 'C' and j == 'G': return -0.5
+    if i == 'C' and k == 'C' and j == 'G': return -0.2
+    if i == 'G' and k == 'C' and j == 'G': return -0.2
+    if i == 'U' and k == 'C' and j == 'G': return -0.1
+
+    if i == 'A' and k == 'G' and j == 'C': return -0.2
+    if i == 'C' and k == 'G' and j == 'C': return -0.2
+    if i == 'G' and k == 'G' and j == 'C': return -0.0
+    if i == 'U' and k == 'G' and j == 'C': return -0.0
+    
+    if i == 'A' and k == 'U' and j == 'A': return -0.3
+    if i == 'C' and k == 'U' and j == 'A': return -0.3
+    if i == 'G' and k == 'U' and j == 'A': return -0.3
+    if i == 'U' and k == 'U' and j == 'A': return -0.2
+
 
 def dangle_Ri():
-    """docstring"""
-    pass
+    """return the scoring parameter for 3' base dangling off a multiloop pair"""
+    return dangle_R + 0.4
+
 
 def dangle_Li():
-    """docstring"""
-    pass
+    """return the scoring parameter for 5' base dangling off a multiloop pair"""
+    return dangle_L + 0.4
 
-def dangle_R_wave():
-    """docstring"""
-    pass
 
-def dangle_L_wave():
-    """docstring"""
-    pass
+def dangle_R_wave(i, j, k):
+    """return the scoring paramater for 3' base dangling off a pseudoknot pair"""
+    return dangle_R(i, j, k) * 0.83 + 0.2
 
-def EIS2_wave():
-    """docstring"""
-    pass
 
-def coaxial_stacking_wave():
-    """docstring"""
-    pass
+def dangle_L_wave(i, j, k):
+    """return the scoring paramater for 5' base dangling off a pseudoknot pair"""
+    return dangle_L(i, k, j) * 0.83 + 0.2
+
+
