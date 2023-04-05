af421cbb49c71aa04777e834e54ff648b6f7c64a
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-05 23:07:17 +0200

    EIS2 function + 2 dic (internal_loop and bulge)

--- src/parameters.py
+++ src/parameters.py
@@ -28,17 +28,122 @@ parameters = {
         "Gwh" : 6.0
 }
 
+bulge_energy = {
+        2 : 5.2,
+        3 : 6.0,
+        4 : 6.7,
+        5 : 7.4,
+        6 : 8.2,
+        7 : 9.1,
+        8 : 10.0,
+        9 : 10.5, 
+        10 : 11.0,
+        11 : 11.4,
+        12 : 11.8,
+        13 : 12.15,
+        14 : 12.5,
+        15 : 12.75,
+        16 : 13.0, 
+        17 : 13.3,
+        18 : 13.6,
+        19 : 13.8,
+        20 : 14.0,
+        21 : 14.2, 
+        22 : 14.4, 
+        23 : 14.6, 
+        24 : 14.8,
+        25 : 15.0,
+        26 : 15.16,
+        27 : 15.32,
+        28 : 15.48,
+        29 : 15.64, 
+        30 : 15.8
+ }
+
+internal_loop_energy = {
+        2 : 0.8,
+        3 : 1.3, 
+        4 : 1.7, 
+        5 : 2.1,
+        6 : 2.5,
+        7 : 2.6,
+        8 : 2.8,
+        9 : 3.1,
+        10: 3.6,
+        11 : 4.0,
+        12 : 4.4,
+        13 : 4.75,
+        14 : 5.1,
+        15 : 5.35,
+        16 : 5.6,
+        17 : 5.9,
+        18 : 6.2, 
+        19 : 6.4,
+        20 : 6.6,
+        21 : 6.8,
+        22 : 7.0,
+        23 : 7.2,
+        24 : 7.4,
+        25 : 7.6,
+        26 : 7.76,
+        27 : 7.92,
+        28 : 8.08,
+        29 : 8.24,
+        30 : 8.4
+}
+
+
 def EIS1(i, j):
-    """docstring"""
+    """scoring function for an irreducible surface of order 1"""
     pass
+    # Nabil 
+
 
 def EIS2(i, j, k, l):
-    """docstring"""
-    pass
+    """scoring function for an irreducible surface of order 2"""
+   
+    #######
+    # k-l #
+    # i-j #
+    # 5 3 #
+    #######
+    
+    # compute number of nucleotides between k and i / j and l 
+    delta_k_i = k - i - 1
+    delta_j_l = j - l - 1
+
+
+    # stem
+    if (delta_k_i == 0) and (delta_j_l == 0):
+        return coaxial_stacking(i, j, k, l)
+
+
+    # bulge
+    if (delta_k_i == 1) or (delta_j_l == 1):
+        # bulge size = 1
+        return coaxial_stacking(i, j, k, l) + 3.3
+
+    if (delta_k_i > 0) and (delta_j_l == 0):
+        # the bulge is between k  and i and its size is delta_k_i
+        if delta_k_i > 30: return 16
+        else: return bulge_energy[delta_k_i]
+
+    if (delta_k_i == 0) and (delta_j_l > 0):
+        # the bulge is between j and l and its size is delta_j_l
+        if delta_j_l > 30: return 16
+        else: return bulge_energy[delta_j_l]
+
+    
+    # internal loop
+    if (delta_k_i > 0) and (delta_k_i > 0):
+        if delta_k_i + delta_j_l > 30: return 8.5
+        else: return internal_loop_energy[delta_k_i + delta_j_l]
+        
 
 def EIS2_wave(i, j, k, l):
     """docstring"""
-    pass
+    return EIS2_wave(i, j, k, l) * 0.83
+
 
 def coaxial_stacking(i, j, k, l):
     """compute and return the coaxial stacking score"""
