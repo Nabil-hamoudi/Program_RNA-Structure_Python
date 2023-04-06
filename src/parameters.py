59175fb181b47f9da700a00a11f0aed45f3ecfa1
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-06 11:55:07 +0200

    Update parameters.py

--- src/parameters.py
+++ src/parameters.py
@@ -142,7 +142,7 @@ def EIS2(i, j, k, l):
 
 def EIS2_wave(i, j, k, l):
     """scoring function for an irreducible surface of order 2 in pseudoknot"""
-    return EIS2_wave(i, j, k, l) * 0.83
+    return EIS2(i, j, k, l) * 0.83
 
 
 def coaxial_stacking(i, j, k, l):
