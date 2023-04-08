fe943f364ffefd2d6554caafe8a9c14f6401ab78
Julie CIESLA <julie.ciesla@ens.uvsq.fr>
2023-04-08 11:35:14 +0200

    parameters+coaxial correct° +dangle equat° +import

--- src/matrix_wx.py
+++ src/matrix_wx.py
@@ -1,4 +1,8 @@
 # Mettre les import
+import matrix_vx
+import matrix_whx
+from parameters import *
+
 
 def matrix_wx(i, j):
     """
@@ -18,28 +22,32 @@ def matrix_wx(i, j):
     best_score = float('inf')
 
     # Paired (9)
-    if score := (P + matrix_vx(i,j)) < best_score: best_score = score
+    if score := (parameters["P"] + matrix_vx(i,j)) < best_score: best_score = score
 
     # Single stranded (9)
-    if score := (Q + matrix_wx(i+1,j)) < best_score: best_score = score
-    if score := (Q + matrix_wx(i, j-1)) < best_score: best_score = score
+    if score := (parameters["Q"] + matrix_wx(i+1,j)) < best_score: best_score = score
+    if score := (parameters["Q"] + matrix_wx(i, j-1)) < best_score: best_score = score
 
     # Nested Bifurcation (9 & 10)
     for k in range(i, j+1):
         if score := (matrix_wx(i,k) + matrix_wx(k+1, j)) < best_score: best_score = score
 
-    # Non nested bifurcation (9)
+    # Non nested bifurcation (pseudoknot) (9)
     for r in range(i, j+1):
         for l in range(i, r+1):
             for k in range(i, l+1):
-                if score := (Gw + matrix_whx(i,r, k,l) + matrix_whx(k+1, j, l-1, r+1)) < best_score: best_score = score
+                if score := (parameters["Gw"] + matrix_whx(i,r, k,l) + matrix_whx(k+1, j, l-1, r+1)) < best_score: best_score = score
 
 
     # Coaxial (11)
-    if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + C(k, i, k+1, j)) < best_score: best_score = score
+    for k in range(i, j+1):
+        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + coaxial_stacking(k, i, k+1, j)) < best_score: best_score = score
 
     # Dangle (12)
-    # Pas fait
+    # Il faudra gérer les erreurs si jamais i+1 ou j-1 sont out of range
+    if score := (dangle_L(i, i+1, j) + matrix_vx(i+1, j)) < best_score: best_score = score
+    if score := (dangle_R(j, i, j-1) + matrix_vx(i, j-1)) < best_score: best_score = score
+    if score := (dangle_L(i, i+1, j-1) + dangle_R(j, i+1, j-1) + matrix_vx(i+1, j-1)) < best_score: best_score = score
 
     # Addition of the best value in the matrix
     wx[j][i] = best_score
