fdad60000657a460d117fcee4fc75b3ee4280e0a
Nabil-hamoudi <nabil.hamoudi@ens.uvsq.fr>
2023-04-09 10:56:13 +0200

    Adding the EIS1 fonction with harpin loop parameters

--- parameters.py
+++ parameters.py
@@ -28,6 +28,37 @@ parameters = {
         "Gwh" : 6.0
 }
 
+harpin_loop_energy = {
+        3 : 7.4,
+        4 : 5.9,
+        5 : 4.4,
+        6 : 4.3,
+        7 : 4.1,
+        8 : 4.1,
+        9 : 4.2,
+        10 : 4.3,
+        11 : 4.6,
+        12 : 4.9,
+        13 : 5.25,
+        14 : 5.6,
+        15 : 5.85,
+        16 : 6.1,
+        17 : 6.4,
+        18 : 6.7,
+        19 : 6.9,
+        20 : 7.1,
+        21 : 7.3,
+        22 : 7.5,
+        23 : 7.7,
+        24 : 7.9,
+        25 : 8.1,
+        26 : 8.26,
+        27 : 8.42,
+        28 : 8.58,
+        29 : 8.74,
+        30 : 8.9
+ }
+
 bulge_energy = {
         2 : 5.2,
         3 : 6.0,
@@ -36,34 +67,34 @@ bulge_energy = {
         6 : 8.2,
         7 : 9.1,
         8 : 10.0,
-        9 : 10.5, 
+        9 : 10.5,
         10 : 11.0,
         11 : 11.4,
         12 : 11.8,
         13 : 12.15,
         14 : 12.5,
         15 : 12.75,
-        16 : 13.0, 
+        16 : 13.0,
         17 : 13.3,
         18 : 13.6,
         19 : 13.8,
         20 : 14.0,
-        21 : 14.2, 
-        22 : 14.4, 
-        23 : 14.6, 
+        21 : 14.2,
+        22 : 14.4,
+        23 : 14.6,
         24 : 14.8,
         25 : 15.0,
         26 : 15.16,
         27 : 15.32,
         28 : 15.48,
-        29 : 15.64, 
+        29 : 15.64,
         30 : 15.8
  }
 
 internal_loop_energy = {
         2 : 0.8,
-        3 : 1.3, 
-        4 : 1.7, 
+        3 : 1.3,
+        4 : 1.7,
         5 : 2.1,
         6 : 2.5,
         7 : 2.6,
@@ -77,7 +108,7 @@ internal_loop_energy = {
         15 : 5.35,
         16 : 5.6,
         17 : 5.9,
-        18 : 6.2, 
+        18 : 6.2,
         19 : 6.4,
         20 : 6.6,
         21 : 6.8,
@@ -95,20 +126,23 @@ internal_loop_energy = {
 
 def EIS1(i, j):
     """scoring function for an irreducible surface of order 1"""
-    pass
-    # Nabil 
+    delta_j_i = j - i - 1
 
+    if delta_j_i > 2:
+        return coaxial_stacking(i, j, i+1, j-1) + harpin_loop_energy[delta_j_i]
+
+    return float("inf")
 
 def EIS2(i, j, k, l):
     """scoring function for an irreducible surface of order 2"""
-   
+
     #######
     # k-l #
     # i-j #
     # 5 3 #
     #######
-    
-    # compute number of nucleotides between k and i / j and l 
+
+    # compute number of nucleotides between k and i / j and l
     delta_k_i = k - i - 1
     delta_j_l = j - l - 1
 
@@ -133,12 +167,12 @@ def EIS2(i, j, k, l):
         if delta_j_l > 30: return 16
         else: return bulge_energy[delta_j_l]
 
-    
+
     # internal loop
     if (delta_k_i > 0) and (delta_k_i > 0):
         if delta_k_i + delta_j_l > 30: return 8.5
         else: return internal_loop_energy[delta_k_i + delta_j_l]
-        
+
 
 def EIS2_wave(i, j, k, l):
     """scoring function for an irreducible surface of order 2 in pseudoknot"""
@@ -147,7 +181,7 @@ def EIS2_wave(i, j, k, l):
 
 def coaxial_stacking(i, j, k, l):
     """compute and return the coaxial stacking score"""
-    
+
     #######
     # --> #
     # i k #
@@ -167,7 +201,7 @@ def coaxial_stacking(i, j, k, l):
     if i == 'C' and j == 'G' and k == 'G' and l == 'C': return -2.0
     if i == 'G' and j == 'C' and k == 'C' and l == 'G': return -3.4
     if i == 'G' and j == 'C' and k == 'G' and l == 'C': return -2.9
-    
+
     # initiation 3.4 ?
 
     # mismatches G-U
@@ -186,8 +220,8 @@ def coaxial_stacking(i, j, k, l):
     if i == 'U' and j == 'G' and k == 'U' and l == 'G': return -0.5
 
     return -0.4
-    
-    
+
+
 
 def coaxial_stacking_wave(i, j, k, l):
     """compute and return the coaxial stacking score in pseudoknots """
@@ -212,7 +246,7 @@ def dangle_R(i, j, k):
     if i == 'C' and k == 'A' and j == 'U': return -0.5
     if i == 'G' and k == 'A' and j == 'U': return -0.8
     if i == 'U' and k == 'A' and j == 'U': return -0.6
-    
+
     if i == 'A' and k == 'C' and j == 'G': return -1.7
     if i == 'C' and k == 'C' and j == 'G': return -0.8
     if i == 'G' and k == 'C' and j == 'G': return -1.7
@@ -222,12 +256,12 @@ def dangle_R(i, j, k):
     if i == 'C' and k == 'G' and j == 'C': return -0.4
     if i == 'G' and k == 'G' and j == 'C': return -1.3
     if i == 'U' and k == 'G' and j == 'C': return -0.6
-    
+
     if i == 'A' and k == 'U' and j == 'A': return -0.7
     if i == 'C' and k == 'U' and j == 'A': return -0.1
     if i == 'G' and k == 'U' and j == 'A': return -0.7
     if i == 'U' and k == 'U' and j == 'A': return -0.1
-    
+
 
 def dangle_L(i, k, j):
     """return the free-energy for unpaired 5' terminal nucleotides """
@@ -246,7 +280,7 @@ def dangle_L(i, k, j):
     if i == 'C' and k == 'A' and j == 'U': return -0.3
     if i == 'G' and k == 'A' and j == 'U': return -0.3
     if i == 'U' and k == 'A' and j == 'U': return -0.2
-    
+
     if i == 'A' and k == 'C' and j == 'G': return -0.5
     if i == 'C' and k == 'C' and j == 'G': return -0.2
     if i == 'G' and k == 'C' and j == 'G': return -0.2
@@ -256,7 +290,7 @@ def dangle_L(i, k, j):
     if i == 'C' and k == 'G' and j == 'C': return -0.2
     if i == 'G' and k == 'G' and j == 'C': return -0.0
     if i == 'U' and k == 'G' and j == 'C': return -0.0
-    
+
     if i == 'A' and k == 'U' and j == 'A': return -0.3
     if i == 'C' and k == 'U' and j == 'A': return -0.3
     if i == 'G' and k == 'U' and j == 'A': return -0.3
