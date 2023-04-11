68e236c79022e96fa1d52a7a705d9bfa06488f08
Julie CIESLA <julie.ciesla@ens.uvsq.fr>
2023-04-11 18:57:11 +0200

    adding coaxial&dangle in pseudoknot+small correct°

--- src/matrix_wx.py
+++ src/matrix_wx.py
@@ -1,4 +1,4 @@
-# Mettre les import
+# Besoin d'avoir accès à la taille de la séquence d'ARN
 import matrix_vx
 import matrix_whx
 from parameters import *
@@ -36,18 +36,28 @@ def matrix_wx(i, j):
     for r in range(i, j+1):
         for l in range(i, r+1):
             for k in range(i, l+1):
-                if score := (parameters["Gw"] + matrix_whx(i,r, k,l) + matrix_whx(k+1, j, l-1, r+1)) < best_score: best_score = score
+                if score := (parameters["Gw"] + matrix_whx(i,r, k,l) + 
+                             matrix_whx(k+1, j, l-1, r+1)) < best_score: best_score = score
 
 
-    # Coaxial (11)
+   # Coaxial OUT OF pseudoknot (11)
     for k in range(i, j+1):
-        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + coaxial_stacking(k, i, k+1, j)) < best_score: best_score = score
+        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + 
+                     coaxial_stacking(k, i, k+1, j)) < best_score: best_score = score
 
+    # Coaxial IN pseudoknot (11)
+    # Pas logique ce sera tjr plus grand que l'équation précédente 
+    for k in range(i, j+1):
+        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + 
+                     coaxial_stacking_wave(k, i, k+1, j)) < best_score: best_score = score
+
+      
     # Dangle (12)
     # Il faudra gérer les erreurs si jamais i+1 ou j-1 sont out of range
-    if score := (dangle_L(i, i+1, j) + matrix_vx(i+1, j)) < best_score: best_score = score
-    if score := (dangle_R(j, i, j-1) + matrix_vx(i, j-1)) < best_score: best_score = score
-    if score := (dangle_L(i, i+1, j-1) + dangle_R(j, i+1, j-1) + matrix_vx(i+1, j-1)) < best_score: best_score = score
+        if score := (dangle_L(i, i+1, j) + matrix_vx(i+1, j)) < best_score: best_score = score
+        if score := (dangle_R(j, i, j-1) + matrix_vx(i, j-1)) < best_score: best_score = score
+        if score := (dangle_L(i, i+1, j-1) + dangle_R(j, i+1, j-1) + 
+                     matrix_vx(i+1, j-1)) < best_score: best_score = score
 
     # Addition of the best value in the matrix
     wx[j][i] = best_score
--- src/matrix_wxi.py
+++ src/matrix_wxi.py
@@ -1,11 +1,11 @@
-# Mettre les import
 from parameters import *
 import matrix_vx
 import matrix_whx
 
+
 def matrix_wxi(i, j):
     """
-    Calculate the box (i,j) of the wx matrix and return its value.
+    Calculate the box (i,j) of the wxi matrix and return its value.
     Entry: i and j: int as i <= j
     Exit: best score value
     """
@@ -40,18 +40,21 @@ def matrix_wxi(i, j):
 
     # Coaxial OUT OF pseudoknot (11)
     for k in range(i, j+1):
-        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + coaxial_stacking(k, i, k+1, j)) < best_score: best_score = score
+        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + 
+                     coaxial_stacking(k, i, k+1, j)) < best_score: best_score = score
 
     # Coaxial IN pseudoknot (11)
-    # Pas logique ce sera tjr plus grand que l'équation précédente 
     for k in range(i, j+1):
-        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + coaxial_stacking_wave(k, i, k+1, j)) < best_score: best_score = score
+        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + 
+                     coaxial_stacking_wave(k, i, k+1, j)) < best_score: best_score = score
 
 
     # Dangle (12)
     if score := (dangle_Li(i, i+1, j) + matrix_vx(i+1, j)) < best_score: best_score = score
     if score := (dangle_Ri(j, i, j-1) + matrix_vx(i, j-1)) < best_score: best_score = score
-    if score := (dangle_Li(i, i+1, j-1) + dangle_Ri(j, i+1, j-1) + matrix_vx(i+1, j-1)) < best_score: best_score = score
+    if score := (dangle_Li(i, i+1, j-1) + dangle_Ri(j, i+1, j-1) + 
+                 matrix_vx(i+1, j-1)) < best_score: best_score = score
+
 
     # Addition of the best value in the matrix
     wxi[j][i] = best_score
