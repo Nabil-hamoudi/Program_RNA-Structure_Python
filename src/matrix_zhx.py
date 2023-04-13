087fec48d2c4552fc01a558d4c8ae402f930b6be
Nabil-hamoudi <nabil.hamoudi@ens.uvsq.fr>
2023-04-13 15:02:17 +0200

    Adding dangles in reccursion fonction matrix_yhx and matrix_zhx also 2 more on each nested bifurcation from "Complete set of recursion for the pseudoknot algorithm"

--- src/matrix_zhx.py
+++ src/matrix_zhx.py
@@ -1,7 +1,8 @@
-from parameters import * 
+from parameters import *
 from matrix_vhx import matrix_vhx
 from matrix_wxi import matrix_wxi
 from matrix_whx import matrix_whx
+from matrix_vx import matrix_vx
 
 
 def matrix_zhx(i, j, k, l):
@@ -12,7 +13,7 @@ def matrix_zhx(i, j, k, l):
 
     if zhx[j][l][k][i] is not None:
         return zhx[j][l][k][i]
-    
+
     if (k+1) == l:
         zhx[j][l][k][i] = matrix_vx(i, j)
         return matrix_vx(i, j)
@@ -23,6 +24,11 @@ def matrix_zhx(i, j, k, l):
     # paired
     if score := (parameters["P_wave"] + matrix_vhx(i, j, k, l)) < best_score: best_score = score
 
+    # dangles
+    if score := (parameters["L_wave"] + parameters["R_wave"] + parameters["P_wave"] + matrix_vhx(i, j, k-1, l+1)) < best_score: best_score = score
+    if score := (parameters["R_wave"] + parameters["P_wave"] + matrix_vhx(i, j, k-1, l)) < best_score: best_score = score
+    if score := (parameters["L_wave"] + parameters["P_wave"] + matrix_vhx(i, j, k, l+1)) < best_score: best_score = score
+
     # single-stranded
     if score := (parameters["Q_wave"] + matrix_zhx(i, j, k-1, l)) < best_score: best_score = score
     if score := (parameters["Q_wave"] + matrix_zhx(i, j, k, l+1)) < best_score: best_score = score
@@ -30,9 +36,11 @@ def matrix_zhx(i, j, k, l):
     # nested bifurcations
     for r in range(i, k+1):
         if score := (matrix_zhx(i, j, r, l) + matrix_wxi(r+1, k)) < best_score: best_score = score
+        if score := ((2 * parameters["P_wave"]) + parameters["C_wave"](r, l, r+1, k) + matrix_vhx(i, j, r, l) + matrix_vx(r+1, k)) < best_score: best_score = score
 
     for s in range(l, j+1):
         if score := (matrix_zhx(i, j, k, s) + matrix_wxi(l, s-1)) < best_score: best_score = score
+        if score := ((2 * parameters["P_wave"]) + parameters["C_wave"](s-1, l, s, k) + matrix_vhx(i, j, k, s) + matrix_vx(l, s-1)) < best_score: best_score = score
 
     for r in range(i, k+1):
         for s in range(l, j+1):
@@ -40,6 +48,6 @@ def matrix_zhx(i, j, k, l):
 
     if score := (parameters["P_wave"] + parameters["M_wave"] + matrix_whx(i + 1, j - 1, k, l)) < best_score: best_score = score
 
-    # store the best score in the matrix and return it 
+    # store the best score in the matrix and return it
     zhx[j][l][k][i] = best_score
     return best_score
