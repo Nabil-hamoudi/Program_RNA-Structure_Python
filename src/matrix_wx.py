2745ab10c012f1c777a68603c0fd0d7c6175909e
Julie CIESLA <julie.ciesla@ens.uvsq.fr>
2023-04-13 13:55:17 +0200

    add all recursions + new import

--- src/matrix_wx.py
+++ src/matrix_wx.py
@@ -1,5 +1,6 @@
 from matrix_vx import matrix_vx
 from matrix_whx import matrix_whx
+from matrix_yhx import matrix_yhx
 from parameters import *
 
 
@@ -20,34 +21,46 @@ def matrix_wx(i, j):
 
     best_score = float('inf')
 
+    # #############################
+    # Beginning Recursions
+
     # Paired (9)
     if score := (parameters["P"] + matrix_vx(i,j)) < best_score: best_score = score
 
+    # Dangles
+    if score := (dangle_L(i, i+1, j-1) + dangle_R(j, i+1, j-1) + parameters["P"]
+                 + matrix_vx(i+1, j-1)) < best_score: best_score = score
+
+    if score := (dangle_L(i, i+1, j) + parameters["P"] + matrix_vx(i+1, j)
+                 ) < best_score: best_score = score
+
+    if score := (dangle_R(j, i, j-1) + parameters["P"] + matrix_vx(i, j-1)
+                 ) < best_score: best_score = score
+
     # Single stranded (9)
-    if score := (parameters["Q"] + matrix_wx(i+1,j)) < best_score: best_score = score
+    if score := (parameters["Q"] + matrix_wx(i+1, j)) < best_score: best_score = score
     if score := (parameters["Q"] + matrix_wx(i, j-1)) < best_score: best_score = score
 
     # Nested Bifurcation (9 & 10)
     for k in range(i, j+1):
-        if score := (matrix_wx(i,k) + matrix_wx(k+1, j)) < best_score: best_score = score
+        if score := (matrix_wx(i, k) + matrix_wx(k+1, j)) < best_score: best_score = score
+
+        if score := (coaxial_stacking(k, i, k+1, j) + matrix_vx(i, k) + matrix_vx(k+1, j)
+                     ) < best_score: best_score = score
 
     # Non nested bifurcation (pseudoknot) (9)
     for r in range(i, j+1):
         for l in range(i, r+1):
             for k in range(i, l+1):
-                if score := (parameters["Gw"] + matrix_whx(i,r, k,l) + 
+                if score := (parameters["Gw"] + matrix_whx(i,r, k,l) +
                              matrix_whx(k+1, j, l-1, r+1)) < best_score: best_score = score
-
-    # Coaxial OUT OF pseudoknot (11)
-    for k in range(i, j+1):
-        if score := (matrix_vx(i, k) + matrix_vx(k+1, j) + 
-                     coaxial_stacking(k, i, k+1, j)) < best_score: best_score = score
-      
-    # Dangle (12)
-    if score := (dangle_L(i, i+1, j) + matrix_vx(i+1, j)) < best_score: best_score = score
-    if score := (dangle_R(j, i, j-1) + matrix_vx(i, j-1)) < best_score: best_score = score
-    if score := (dangle_L(i, i+1, j-1) + dangle_R(j, i+1, j-1) + 
-                matrix_vx(i+1, j-1)) < best_score: best_score = score
+          
+                if score := (2 * parameters["P_wave"] + parameters["Gw"] 
+                             + coaxial_stacking_wave(l-1, r+1, l, k) + matrix_yhx(i, r, k, l)
+                             + matrix_yhx(k+1, j, l-1, r+1)) < best_score: best_score = score
+ 
+    # End Recursions
+    # #############################
 
     # Addition of the best value in the matrix
     wx[j][i] = best_score
