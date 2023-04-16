5904ccc51ce150d66a80a9acdd8637b66df85272
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-16 21:28:54 +0200

    Update matrix_vhx.py

--- src/matrix/matrix_vhx.py
+++ src/matrix/matrix_vhx.py
@@ -3,13 +3,14 @@ import matrix_whx
 
 # matrix vhx
 
-def matrix_vhx(i, j, k, l, matrix):
+def matrix_vhx(i, j, k, l, matrix, sequence):
     """Return the value of the gap matrix vhx at the given indices"""
 
     #check indices
     if (i > k) or (k > l) or (l > j):
-        print(f"Error : invalid index. \n i = {i}, k = {k}, l = {l}, j = {j}")
-        exit(1)
+        # print(f"Error : invalid index. \n i = {i}, k = {k}, l = {l}, j = {j}")
+        # exit(1)
+        return float('inf')
 
     if matrix["vhx"][j][l][k][i] is not None:
         return matrix["vhx"][j][l][k][i]
@@ -21,13 +22,13 @@ def matrix_vhx(i, j, k, l, matrix):
 
     # search for a better score
     if score := parameters["EIS2_wave"](i, j, k, l) < best_score: best_score = score
-    if score := (2 * parameters["P_wave"] + parameters["M_wave"] + matrix_whx.matrix_whx(i+1, j-1, k-1, l+1, matrix)) < best_score: best_score = score
+    if score := (2 * parameters["P_wave"] + parameters["M_wave"] + matrix_whx.matrix_whx(i+1, j-1, k-1, l+1, matrix, sequence)) < best_score: best_score = score
 
 
     for s in range(l, j+1):
         for r in range(i, k+1):
-            if score := parameters["EIS2_wave"](i, j, r, s) + matrix_vhx(r, s, k, l, matrix) < best_score: best_score = score
-            if score := parameters["EIS2_wave"](r, s, k, l) + matrix_vhx(i, j, r, s, matrix) < best_score: best_score = score
+            if score := parameters["EIS2_wave"](i, j, r, s) + matrix_vhx(r, s, k, l, matrix, sequence) < best_score: best_score = score
+            if score := parameters["EIS2_wave"](r, s, k, l) + matrix_vhx(i, j, r, s, matrix, sequence) < best_score: best_score = score
 
 
     # store the best score in the matrix and return it 
