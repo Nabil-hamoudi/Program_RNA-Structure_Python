a562d02e14eaa6cec2f6b412e73a0326f8c45572
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-16 21:56:40 +0200

    Update matrix_whx.py

--- src/matrix/matrix_whx.py
+++ src/matrix/matrix_whx.py
@@ -44,61 +44,61 @@ def matrix_whx(i, j, k, l, matrix, sequence):
 
 
     # dangles 
-    if score := parameters["L_wave"](i, i+1, j) + parameters["R_wave"](k, k-1, l) \
+    if score := parameters["L_wave"](i, i+1, j, sequence) + parameters["R_wave"](k, k-1, l, sequence) \
             + (2 * parameters["P_wave"]) + matrix_vhx.matrix_vhx(i+1, j, k-1, l, matrix, sequence) < best_score:
                 best_score = score
 
-    if score := parameters["L_wave"](l-1, k, l) + parameters["R_wave"](j, i, j-1) \
+    if score := parameters["L_wave"](l-1, k, l, sequence) + parameters["R_wave"](j, i, j-1, sequence) \
             + (2 * parameters["P_wave"]) + matrix_vhx.matrix_vhx(i, j-1, k, l+1, matrix, sequence) < best_score:
                 best_score = score
     
-    if score := (parameters["L_wave"](i, i+1, j) + parameters["L_wave"](l, k, l+1)) \
+    if score := (parameters["L_wave"](i, i+1, j, sequence) + parameters["L_wave"](l, k, l+1, sequence)) \
             + (2 * parameters["P_wave"]) + matrix_vhx.matrix_vhx(i+1, j, k, l+1, matrix, sequence) < best_score:
                 best_score = score
     
-    if score := (parameters["R_wave"](k, k-1, l) + parameters["R_wave"](j, i, j-1)) \
+    if score := (parameters["R_wave"](k, k-1, l, sequence) + parameters["R_wave"](j, i, j-1, sequence)) \
             + (2 * parameters["P_wave"]) + matrix_vhx.matrix_vhx(i, j-1, k-1, l, matrix, sequence) < best_score:
                 best_score = score
     
-    if score := parameters["L_wave"](i, i+1, j-1) + (parameters["R_wave"](k, k-1, l) \
-            + parameters["R_wave"](j, i+1, j-1)) + (2 * parameters["P_wave"]) \
+    if score := parameters["L_wave"](i, i+1, j-1, sequence) + (parameters["R_wave"](k, k-1, l, sequence) \
+            + parameters["R_wave"](j, i+1, j-1, sequence)) + (2 * parameters["P_wave"]) \
             + matrix_vhx.matrix_vhx(i+1, j-1, k-1, l, matrix, sequence) < best_score: best_score = score
     
-    if score := (parameters["L_wave"](i, i+1, j) + parameters["L_wave"](l, k-1, l+1)) \
-            + parameters["R_wave"](k, k-1, l+1) + (2 * parameters["P_wave"]) \
+    if score := (parameters["L_wave"](i, i+1, j, sequence) + parameters["L_wave"](l, k-1, l+1, sequence)) \
+            + parameters["R_wave"](k, k-1, l+1, sequence) + (2 * parameters["P_wave"]) \
             + matrix_vhx.matrix_vhx(i+1, j, k-1, l+1, matrix, sequence) < best_score: best_score = score
     
-    if score := (parameters["L_wave"](i, i+1, j-1) + parameters["L_wave"](l, k, l+1)) \
-            + parameters["R_wave"](j, i+1, j-1) + (2 * parameters["P_wave"]) \
+    if score := (parameters["L_wave"](i, i+1, j-1, sequence) + parameters["L_wave"](l, k, l+1, sequence)) \
+            + parameters["R_wave"](j, i+1, j-1, sequence) + (2 * parameters["P_wave"]) \
             + matrix_vhx.matrix_vhx(i+1, j-1, k, l+1, matrix, sequence) < best_score: best_score = score
     
-    if score := parameters["L_wave"](l, k-1, l+1) + (parameters["R_wave"](k, k-1, l+1) \
-            + parameters["R_wave"](j, i, j-1)) + (2 * parameters["P_wave"]) \
+    if score := parameters["L_wave"](l, k-1, l+1, sequence) + (parameters["R_wave"](k, k-1, l+1, sequence) \
+            + parameters["R_wave"](j, i, j-1, sequence)) + (2 * parameters["P_wave"]) \
             + matrix_vhx.matrix_vhx(i, j-1, k-1, l+1, matrix, sequence) < best_score: best_score = score
     
-    if score := (parameters["L_wave"](i, i+1, j-1) + parameters["L_wave"](l, k-1, l+1)) \
-            + (parameters["R_wave"](k, k-1, l+1) + parameters["R_wave"](j, i+1, j-1)) \
+    if score := (parameters["L_wave"](i, i+1, j-1, sequence) + parameters["L_wave"](l, k-1, l+1, sequence)) \
+            + (parameters["R_wave"](k, k-1, l+1, sequence) + parameters["R_wave"](j, i+1, j-1, sequence)) \
             + (2 * parameters["P_wave"]) + matrix_vhx.matrix_vhx(i+1, j-1, k-1, l+1, matrix, sequence) < best_score:
                 best_score = score
 
-    if score := parameters["L_wave"](i, i+1, j-1) + parameters["R_wave"](j, i+1, j-1) \
+    if score := parameters["L_wave"](i, i+1, j-1, sequence) + parameters["R_wave"](j, i+1, j-1, sequence) \
             + parameters["P_wave"] + matrix_zhx.matrix_zhx(i+1, j-1, k, l, matrix, sequence) < best_score:
                 best_score = score
     
-    if score := parameters["L_wave"](i, i+1, j) + parameters["P_wave"] \
+    if score := parameters["L_wave"](i, i+1, j, sequence) + parameters["P_wave"] \
             + matrix_zhx.matrix_zhx(i+1, j, k, l, matrix, sequence) < best_score: best_score = score
 
-    if score := parameters["R_wave"](j, i, j-1) + parameters["P_wave"] \
+    if score := parameters["R_wave"](j, i, j-1, sequence) + parameters["P_wave"] \
             + matrix_zhx.matrix_zhx(i, j-1, k, l, matrix, sequence) < best_score: best_score = score
     
-    if score := parameters["L_wave"](l, k-1, l+1) + parameters["R_wave"](k, k-1, l+1) \
+    if score := parameters["L_wave"](l, k-1, l+1, sequence) + parameters["R_wave"](k, k-1, l+1, sequence) \
             + parameters["P_wave"] + matrix_yhx.matrix_yhx(i, j, k-1, l+1, matrix, sequence) < best_score:
                 best_score = score
     
-    if score := parameters["R_wave"](k, k-1, l) + parameters["P_wave"] \
+    if score := parameters["R_wave"](k, k-1, l, sequence) + parameters["P_wave"] \
             + matrix_yhx.matrix_yhx(i, j, k-1, l+1, matrix, sequence) < best_score: best_score = score
     
-    if score := parameters["L_wave"](l, k, l+1) + parameters["P_wave"] \
+    if score := parameters["L_wave"](l, k, l+1, sequence) + parameters["P_wave"] \
             + matrix_yhx.matrix_yhx(i, j, k, l+1, matrix, sequence) < best_score: best_score = score
 
 
@@ -111,19 +111,19 @@ def matrix_whx(i, j, k, l, matrix, sequence):
     for r in range(i, k+1):
         if score := (matrix_wxi.matrix_wxi(i, r, matrix, sequence) + matrix_whx(r+1, j, k, l, matrix, sequence)) < best_score: best_score = score
         
-        if score := 2 * parameters["P_wave"] + coaxial_stacking(r, i, r+1, j) \
+        if score := 2 * parameters["P_wave"] + coaxial_stacking(r, i, r+1, j, sequence) \
                 + matrix_vx.matrix_vx(i, r, matrix, sequence) + matrix_zhx.matrix_zhx(r+1, j, k, l, matrix, sequence) < best_score: best_score = score
 
         if score := (matrix_wxi.matrix_wxi(r+1, k, matrix, sequence) + matrix_whx(i, j, r, l, matrix, sequence)) < best_score: best_score = score
 
 
     for s in range(l, j+1):
-        if score := 2 * parameters["P_wave"] + coaxial_stacking(r, l, r+1, k) \
+        if score := 2 * parameters["P_wave"] + coaxial_stacking(r, l, r+1, k, sequence) \
                 + matrix_yhx.matrix_yhx(i, j, r, l, matrix, sequence) + matrix_vx.matrix_vx(r+1, k, matrix, sequence) < best_score: best_score = score
 
         if score := (matrix_wxi.matrix_wxi(s+1, j, matrix, sequence) + matrix_whx(i, s, k, l, matrix, sequence)) < best_score: best_score = score
 
-        if score := 2 * parameters["P_wave"] + coaxial_stacking(s, i, s+1, j) \
+        if score := 2 * parameters["P_wave"] + coaxial_stacking(s, i, s+1, j, sequence) \
                 + matrix_zhx.matrix_zhx(i, s, k, l, matrix, sequence) + matrix_vx.matrix_vx(s+1, j, matrix, sequence) < best_score: best_score = score
 
         if score := (matrix_wxi.matrix_wxi(l, s, matrix, sequence) + matrix_whx(i, j, k, s+1, matrix, sequence)) < best_score: best_score = score
@@ -131,7 +131,7 @@ def matrix_whx(i, j, k, l, matrix, sequence):
 
     for s in range(l, j+1):
         for r in range(i, k+1):
-            if score := 2 * parameters["P_wave"] + coaxial_stacking(s, l, s+1, k) \
+            if score := 2 * parameters["P_wave"] + coaxial_stacking(s, l, s+1, k, sequence) \
                     + matrix_yhx.matrix_yhx(i, j, k, s+1, matrix, sequence) + matrix_vx.matrix_vx(l, s, matrix, sequence) < best_score: best_score = score
 
             if score (matrix_yhx.matrix_yhx(i, j, r, s, matrix, sequence) +  matrix_zhx.matrix_zhx(r, s,  k, l, matrix, sequence)) < best_score: best_score = score
@@ -153,12 +153,12 @@ def matrix_whx(i, j, k, l, matrix, sequence):
                             + matrix_whx(l, j, s-1, s_prime+1, matrix, sequence)) < best_score: best_score = score
                     
                     if score := 2 * parameters["P_wave"] + parameters["Gwh"] \
-                            + coaxial_stacking_wave(s_prime, i, s_prime + 1, s-1) \
+                            + coaxial_stacking_wave(s_prime, i, s_prime + 1, s-1, sequence) \
                             + matrix_zhx.matrix_zhx(i, s_prime, k, s, matrix, sequence) + matrix_yhx.matrix_yhx(l, j, s-1, s_prime + 1, matrix, sequence) \
                             < best_score: best_score = score
 
                     if score := 2 * parameters["P_wave"] + parameters["Gwh"] \
-                            + coaxial_stacking_wave(s-1, s_prime + 1, s, k) \
+                            + coaxial_stacking_wave(s-1, s_prime + 1, s, k, sequence) \
                             + matrix_yhx.matrix_yhx(i, s_prime, k, s, matrix, sequence) + matrix_yhx.matrix_yhx(l, j, s-1, s_prime + 1, matrix, sequence) \
                             < best_score: best_score = score
 
@@ -166,12 +166,12 @@ def matrix_whx(i, j, k, l, matrix, sequence):
                             +  matrix_whx(i, k, r-1, r_prime+1, matrix, sequence) < best_score: best_score = score
 
                     if score := 2 * parameters["P_wave"] + parameters["Gwh"] \
-                            + coaxial_stacking_wave(r-1, r_prime+1, r, j) \
+                            + coaxial_stacking_wave(r-1, r_prime+1, r, j, sequence) \
                             + matrix_zhx.matrix_zhx(r, j, r_prime, l, matrix, sequence) + matrix_yhx.matrix_yhx(i, k, r-1, r_prime + 1, matrix, sequence) \
                             < best_score: best_score = score
 
                     if score := 2 * parameters["P_wave"] + parameters["Gwh"] \
-                            + coaxial_stacking_wave(r_prime, l, r_prime + 1, r-1) \
+                            + coaxial_stacking_wave(r_prime, l, r_prime + 1, r-1, sequence) \
                             + matrix_yhx.matrix_yhx(r, j, r_prime, l, matrix, sequence) + matrix_yhx.matrix_yhx(i, k, r-1, r_prime + 1, matrix, sequence) \
                             < best_score: best_score = score
 
