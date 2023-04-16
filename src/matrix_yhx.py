11e2623b1fc4a93b02ab384bb01e795a2564afa5
Julie CIESLA <julie.ciesla@ens.uvsq.fr>
2023-04-16 18:23:47 +0200

    ?

--- src/matrix_yhx.py
+++ src/matrix_yhx.py
@@ -1,7 +1,8 @@
-from parameters import * 
+from parameters import *
 from matrix_vhx import matrix_vhx
 from matrix_wxi import matrix_wxi
 from matrix_whx import matrix_whx
+from matrix_vx import matrix_vx
 
 
 def matrix_yhx(i, j, k, l):
@@ -19,6 +20,11 @@ def matrix_yhx(i, j, k, l):
     # paired
     if score := (parameters["P_wave"] + matrix_vhx(i, j, k, l)) < best_score: best_score = score
 
+    # dangles
+    if score := (parameters["L_wave"](i, i+1, j-1) + parameters["R_wave"](j, i+1, j-1) + parameters["P_wave"] + matrix_vhx(i+1, j-1, k, l)) < best_score: best_score = score
+    if score := (parameters["L_wave"](i, i+1, j) + parameters["P_wave"] + matrix_vhx(i+1, j, k, l)) < best_score: best_score = score
+    if score := (parameters["R_wave"](j, i, j-1) + parameters["P_wave"] + matrix_vhx(i, j-1, k, l)) < best_score: best_score = score
+
     # single-stranded
     if score := (parameters["Q_wave"] + matrix_yhx(i + 1, j, k, l)) < best_score: best_score = score
     if score := (parameters["Q_wave"] + matrix_yhx(i, j - 1, k, l)) < best_score: best_score = score
@@ -26,9 +32,11 @@ def matrix_yhx(i, j, k, l):
     # nested bifurcations
     for r in range(i, k+1):
         if score := (matrix_wxi(i, r) + matrix_yhx(r+1, j, k, l)) < best_score: best_score = score
+        if score := ((2 * parameters["P_wave"]) + parameters["C_wave"](r, i, r+1, j) + matrix_vx(i, r) + matrix_vhx(r+1, j, k, l)) < best_score: best_score = score
 
     for s in range(l, j+1):
         if score := (matrix_yhx(i, s, k, l) + matrix_wxi(s+1, j)) < best_score: best_score = score
+        if score := ((2 * parameters["P_wave"]) + parameters["C_wave"](s, i, s+1, j) + matrix_vhx(i, s, k, l) + matrix_vx(s+1, j)) < best_score: best_score = score
 
     for r in range(i, k+1):
         for s in range(l, j+1):
@@ -36,6 +44,6 @@ def matrix_yhx(i, j, k, l):
 
     if score := (parameters["P_wave"] + parameters["M_wave"] + matrix_whx(i, j, k - 1, l + 1)) < best_score: best_score = score
 
-    # store the best score in the matrix and return it 
+    # store the best score in the matrix and return it
     yhx[j][l][k][i] = best_score
     return best_score
