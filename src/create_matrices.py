efb2990320a8417cbe2d927493a8bbf93355e70f
Corbo <71632547+petitcorbo@users.noreply.github.com>
2023-04-24 09:57:45 +0200

    files restructuration

--- src/create_matrices.py
+++ src/create_matrices.py
@@ -2,23 +2,27 @@
 
 def create_matrices(n):
     """creation of the six triangular matrices """
-    global vx, wx, vhx, whx, yhx, zhx
+    # global vx, wx, vhx, whx, yhx, zhx
     vx = [[None for i in range(j+1)] for j in range(n)]
     wx = [[None for i in range(j+1)] for j in range(n)]
+    wxi = [[None for i in range(j+1)] for j in range(n)]
     vhx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
     whx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
     yhx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
     zhx = [[[[None for i in range(k+1)] for k in range(l+1)] for l in range(j+1)] for j in range(n)]
 
+    return {"vx" : vx, "wx" : wx, "wxi" : wxi, "vhx" : vhx, "whx" : whx, "yhx" : yhx, "zhx" : zhx}
 
-def fill_matrices():
+
+def fill_matrices(matrix):
     """fill the matrices with the initialization conditions"""
-    n = len(vx)
+    n = len(matrix["vx"])
 
     # initialization for vx and wx
     for i in range(n):
-        vx[i][i] = float('inf')
-        wx[i][i] = 0
+        matrix["vx"][i][i] = (float('inf'), [])
+        matrix["wx"][i][i] = (0, [])
+        matrix["wxi"][i][i] = (0, [])
 
     #initialization for vhx, whx, yhx, zhx
     for j in range(n):
@@ -26,28 +30,25 @@ def fill_matrices():
             for i in range(k+1):
 
                 #vhx
-                vhx[j][k][k][i] = float('inf')
+                matrix["vhx"][j][k][k][i] = (float('inf'), [])
                 
                 #yhx
-                yhx[j][k][k][i] = float('inf')
+                matrix["yhx"][j][k][k][i] = (float('inf'), [])
                 
                 # whx
-                whx[j][k][k][i] = wx[j][i]
-                whx[j][j][i][i] = float('inf')
+                matrix["whx"][j][k][k][i] = matrix["wx"][j][i]
+                matrix["whx"][j][j][i][i] = (float('inf'), [])
                 #k+1 must be lower than j
                 if k+1 <= j:
-                    whx[j][k+1][k][i] = wx[j][i]
+                    matrix["whx"][j][k+1][k][i] = matrix["wx"][j][i]
                 
                 #zhx
-                zhx[j][k][k][i] = vx[j][i]
+                matrix["zhx"][j][k][k][i] = matrix["vx"][j][i]
                 #k+1 must be lower than j 
                 if k+1 <= j:
-                    zhx[j][k+1][k][i] = vx[j][i]
+                    matrix["zhx"][j][k+1][k][i] = matrix["vx"][j][i]
                 
 
                 
-
-create_matrices(4)
-fill_matrices()
 #for l in vx: print(l)
 #for l in wx: print(l)
