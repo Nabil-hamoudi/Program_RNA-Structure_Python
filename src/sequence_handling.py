65e838b2c351a330e05b95c0dce14f42d44b2956
Corbo <71632547+petitcorbo@users.noreply.github.com>
2023-04-27 12:07:49 +0200

    Create sequence_handling.py

---
+++ src/sequence_handling.py
@@ -0,0 +1,58 @@
+def reading_fasta_file(file):
+    """
+    Reading file containing RNA (or DNA) sequence
+    Input: file in fasta format
+    Output: dictionary with RNA (or DNA) sequence(s) & sequence(s) name
+            Under the form {name_seq1: seq1, name_seq2: seq2}
+    """
+    sequences = {}
+    counter_unknow_seq = 1
+    counter_same_name = 1
+
+    with file as f:
+        sequence_name = ""
+        sequence = ""
+        for line in f:  # iterate through each line of the file
+            line = line.strip()  # removing both the leading and the trailing characters of the line
+            if line.startswith(">"):  # if sequence header
+                if sequence_name != "":
+                    # If 2 different RNA sequences have the same name
+                    if sequence_name in sequences.keys() and sequences[sequence_name] != sequence:
+                        sequence_name = sequence_name + str(counter_same_name)
+                        counter_same_name += 1
+
+                    sequences[sequence_name] = sequence  # add previous sequence to dictionary
+                    sequence = ""
+                sequence_name = line[1:]
+                if sequence_name == "":  # if sequence without informations/header
+                    sequence_name = "Unknow sequence " + str(counter_unknow_seq)
+                    counter_unknow_seq += 1
+            else:  # if it is a sequence line
+                sequence += line
+
+        # If 2 different RNA sequences have the same name
+        if sequence_name in sequences.keys() and sequences[sequence_name] != sequence:
+            sequence_name = sequence_name + str(counter_same_name)
+            counter_same_name += 1
+        sequences[sequence_name] = sequence # add previous sequence to dictionary
+    return sequences
+
+
+def check_rna_seq(sequence):
+    """
+    Check if it's an RNA sequence.
+    If it's a DNA sequence, transform it into an RNA sequence.
+    Otherwise return an error.
+    input: RNA or DNA sequence
+    output: RNA sequence
+    """
+    sequence = sequence.upper()
+    list_nucleotides = ["A", "T", "G", "C", "U"]
+    for i in range(len(sequence)):
+        # replacing T by U
+        if sequence[i] == "T":
+            sequence = sequence[:i] + "U" + sequence[i+1:]
+        elif sequence[i] not in list_nucleotides:
+            raise ValueError("The sequence entered is not an RNA or DNA sequence")
+
+    return sequence
