cd7f2fa2d8f120ddade0f3446487028cd5fe2653
Corbo <71632547+petitcorbo@users.noreply.github.com>
2023-04-29 11:30:34 +0200

    restructuration + preparation for file to run

--- src/main.py
+++ src/main.py
@@ -4,28 +4,22 @@ from sequence_handling import *
 from create_matrices import *
 from matrices import matrix_wx
 from program_parser import parser_function
+import os
 
 # Default constant for file display
 GRAPH_EXTENSION = ".jpeg"
 
 
-def program_parse(args):
+def get_sequences(args):
     """
     Input: Arguments entered by the user
     Output: 
     Manage the program with the arguments
     """
     if args.input is not None:
-        sequence = check_rna_seq(args.input)
-        sequence_name = "Unknown sequence"
-        output = get_output(sequence, sequence_name, args.traceback, args.graph)
+        return {"Unknown sequence": args.input}
     else:
-        dict_seq = reading_fasta_file(args.file_input)
-        output = ""
-        for sequence_name in dict_seq:
-            sequence = check_rna_seq(dict_seq[sequence_name])
-            output += get_output(sequence, sequence_name, args.traceback, args.graph)
-    return output
+        return reading_fasta_file(args.file_input)
 
 
 def get_output(sequence, sequence_name, verbose_traceback, graphe_directory=None):
@@ -33,14 +27,14 @@ def get_output(sequence, sequence_name, verbose_traceback, graphe_directory=None
     Output str of the save if need it and 
     save graph(s) if graph argument present
     """
-    matches, best_score = run_programs(sequence, verbose_traceback)
-    output = display_results(sequence_name, sequence, matches, best_score) + '\n'
+    matches, best_score = run_algorithm(sequence, verbose_traceback)
+    output = display_results(sequence_name, sequence, matches, best_score)
     if graphe_directory is not None:
         draw_graph(os.path.join(graphe_directory, sequence_name + GRAPH_EXTENSION), sequence, matches)
     return output
 
 
-def run_programs(sequence, verbose=False):
+def run_algorithm(sequence, verbose=False):
     """
     Start the dynamic programming algorithm
     """
@@ -52,7 +46,7 @@ def run_programs(sequence, verbose=False):
     matrix_wx.matrix_wx(0, len(sequence)-1, matrix, sequence)
 
     # traceback
-    matches = ["_"] * len(sequence)
+    matches = [None] * len(sequence)
     traceback(matrix, "wx", (0, len(sequence) - 1), matches, verbose)
 
     best_score = matrix["wx"][len(sequence) - 1][0][0]
@@ -60,10 +54,20 @@ def run_programs(sequence, verbose=False):
     return (matches, best_score)
 
 
-if __name__ == "__main__":
+def main():
     args = parser_function()
-    output = program_parse(args)
+    
+    dict_seq = get_sequences(args)
+
+    output = ""
+    for sequence_name in dict_seq:
+        sequence = check_rna_seq(dict_seq[sequence_name])
+        output += get_output(sequence, sequence_name, args.traceback, args.graph)
 
     # write into file if asked
     if args.save is not None:
         args.save.write(output)
+
+
+if __name__ == "__main__":
+    main()
