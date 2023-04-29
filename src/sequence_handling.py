b19c24ded8f183e7d79c7cd5838eb3a175213b94
Chloé <71597811+chloegdt@users.noreply.github.com>
2023-04-29 12:31:41 +0200

    add WARNING

--- src/sequence_handling.py
+++ src/sequence_handling.py
@@ -58,6 +58,7 @@ def reading_fasta_file(file):
 
 def check_rna_seq(sequence, sequence_name):
     """
+    Check if the sequence isn't empty.
     Check if it's an RNA sequence.
     If it's a DNA sequence, transform it into an RNA sequence.
     Otherwise return an error.
@@ -67,7 +68,7 @@ def check_rna_seq(sequence, sequence_name):
     
     # check if sequence is empty
     if sequence == "":
-        print(f"The sequence {sequence_name} is empty")
+        print(f"WARNING : The sequence {sequence_name} is empty\n")
         return ""
   
     sequence = sequence.upper()
@@ -77,7 +78,7 @@ def check_rna_seq(sequence, sequence_name):
         if sequence[i] == "T":
             sequence = sequence[:i] + "U" + sequence[i+1:]
         elif sequence[i] not in list_nucleotides:
-            print(f"The sequence {sequence_name} is not an RNA or DNA sequence") 
+            print(f"WARNING : The sequence {sequence_name} is not an RNA or DNA sequence\n") 
             return ""
         
 
